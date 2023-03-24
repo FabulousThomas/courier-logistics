4d628f817d837dac6a2aecf72faf1920ebb7cfd2
Mba Thomas <angelfab631@gmail.com>
2023-03-24 10:15:53 +0100

    Create navbar.php

---
+++ inc/navbar.php
@@ -0,0 +1,35 @@
+ <!-- NAV START -->
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-0 py-3 fixed-top shadow black-bg">
+   <div class="container text-dark">
+     <a href="./index.php">
+       <img src="./assets/img/logo/logo.png" width="100px" alt="LOGO">
+     </a>
+     <button class="navbar-toggler d-lg-none d-inline theme-color" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon theme-color"></span>
+     </button>
+     <div class="collapse navbar-collapse" id="collapsibleNavId">
+       <div class="m-auto"></div>
+       <ul class="navbar-nav ms-auto">
+         <li class="nav-item">
+           <a class="nav-link" href="./index.php">Home</a>
+         </li>
+         <li class="nav-item dropdown">
+           <a class="nav-link" href="./index.php#services">Services</a>
+         </li>
+         <li class="nav-item dropdown">
+           <a class="nav-link" href="./index.php#about">About</a>
+         </li>
+
+         <li class="nav-item">
+           <a class="nav-link" href="./index.php#contact">Contact</a>
+         </li>
+
+         <li class="nav-item">
+           <a class="nav-link" href="./track-package.php">Track</a>
+         </li>
+
+       </ul>
+     </div>
+   </div>
+ </nav>
+ <!-- NAV END -->
