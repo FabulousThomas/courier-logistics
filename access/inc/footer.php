216f422985c074df1a7aecede8ab91d2bfedf8a3
Mba Thomas <angelfab631@gmail.com>
2023-03-25 15:43:21 +0100

    Create footer.php

---
+++ access/inc/footer.php
@@ -0,0 +1,9 @@
+<?php require_once('modal.php'); ?>
+
+<!-- JS -->
+<script src="./assets/js/jquery-3.6.1.min.js"></script>
+<script src="./assets/js/bootstrap-5.2.3.bundle.min.js"></script>
+
+</body>
+
+</html>
