d8836b8bf69714528929bdf1d0677fcf81353e40
Mba Thomas <angelfab631@gmail.com>
2023-03-25 15:42:12 +0100

    Create config.php

---
+++ access/server/config.php
@@ -0,0 +1,20 @@
+<?php
+// DB config
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'maxcourier');
+
+// Root
+define('APPROOT', dirname(dirname(dirname(__FILE__))));
+// App root
+define('ADMINROOT', dirname(dirname(__FILE__)));
+// Url Root
+define('URLROOT', 'http://localhost/maxcourier');
+// Site Name
+define('SITENAME', 'MaxExpress Services');
+// Site Logo
+define('SITELOGO', '<h5 class="text-uppercase mb-0" style="color: white;">-FreeTown&acd;</h5>
+<h5 class="text-uppercase mb-0 text-white"> &ThinSpace; &ThinSpace; &ThinSpace; Hotel&Suit-</h5>');
+
+// die(APPROOT);
