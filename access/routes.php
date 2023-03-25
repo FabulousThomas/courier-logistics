b71d6a615143e98d49ba6c753a71acd4f535a73e
Mba Thomas <angelfab631@gmail.com>
2023-03-25 15:42:05 +0100

    Create routes.php

---
+++ access/routes.php
@@ -0,0 +1,41 @@
+<?php
+require './inc/head.php';
+require './inc/navbar.php';
+$i = 1;
+
+$getRoutes = selectAll('products');
+$row = mysqli_fetch_assoc($getRoutes);
+?>
+
+<main class="py-5">
+
+   <div class="container">
+      <h2 class="text-center mb-0">Manage Routes</h2>
+      <p class="text-center">Route gives you the privilege to manage locations of transit of this product.</p>
+      <div class="col-lg-8 col-md-12 mx-auto">
+         <p>Choose product to manage routes</p>
+         <form action="add-routes.php?p_id=<?= $row['p_id'] ?>" method="GET">
+            <div class="form-group mb-3">
+               <select class="form-select form-control" name="p_id">
+                  <!-- <option selected disabled>Select Route To Manage</option> -->
+                  <?php if (mysqli_num_rows($getRoutes) > 0) : ?>
+                     <?php foreach ($getRoutes as $row) : ?>
+                        <option value="<?= $row['p_id'] ?>"><?= $i++ . '. ' . $row['r_name'] ?></option>
+                     <?php endforeach; ?>
+                  <?php else : ?>
+                     <option disabled>No data found</option>
+                  <?php endif; ?>
+               </select>
+            </div>
+            <div class="form-group mb-3">
+               <button type="submit" class="btn btn-secondary">Proceed to edit</button>
+            </div>
+         </form>
+      </div>
+   </div>
+
+</main>
+
+<?php
+require './inc/footer.php';
+?>
