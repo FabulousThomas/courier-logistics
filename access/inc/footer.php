15c1ad7d94167b209de350fb8353d8beeeb7519b
Mba Thomas <angelfab631@gmail.com>
2023-03-26 07:46:34 +0200

    Update footer.php

--- access/inc/footer.php
+++ access/inc/footer.php
@@ -3,6 +3,8 @@
 <!-- JS -->
 <script src="./assets/js/jquery-3.6.1.min.js"></script>
 <script src="./assets/js/bootstrap-5.2.3.bundle.min.js"></script>
+<script src="./assets/js/script.js"></script>
+
 
 </body>
 
