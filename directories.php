c2d76fa7d1eefd2038902c19c94c7c7f00e20233
Mba Thomas <angelfab631@gmail.com>
2023-03-26 07:39:48 +0200

    Create directories.php

---
+++ directories.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+global $conn;
+require  'access/server/config.php';
+require 'access/server/connection.php';
+require 'access/server/helpers.php';
+require 'access/server/logics.php';
