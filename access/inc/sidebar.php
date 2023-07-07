39ea5fc68f6a89ce539e6991aa29dc28d40d6d80
Mba Thomas <angelfab631@gmail.com>
2023-07-07 03:34:25 -0500

    Create sidebar.php

---
+++ access/inc/sidebar.php
@@ -0,0 +1,39 @@
+<div class="offcanvas offcanvas-start text-bg-dark" data-bs-scroll="false" data-bs-backdrop="true" tabindex="-1"
+   id="offcanvas" aria-labelledby="offcanvasExampleLabel">
+   <div class="offcanvas-header">
+      <a href="./">
+         <img src="./assets/img/logo/logo.png" width="100px" alt="LOGO">
+      </a>
+      <button type="button" class="btn-close text-reset shadow-none text-white" data-bs-dismiss="offcanvas"
+         aria-label="Close"></button>
+   </div>
+   <div class="offcanvas-body">
+      <ul class="">
+         <li class="py-2">
+            <a class="" href="./">Home</a>
+         </li>
+         <li class="py-2">
+            <a class="" href="./">Manage Package</a>
+         </li>
+         <li class="py-2">
+            <a class="" type="button" data-bs-toggle="modal" data-bs-target="#add-product">Add Package</a>
+         </li>
+         <li class="py-2">
+            <a class="" href="./routes.php">Manage Routes</a>
+         </li>
+         <li class="py-2">
+            <a class="" href="./view.php">Print Receipt</a>
+         </li>
+         <!-- <li class="py-2">
+           <a class="" href="./settings.php">Website Settings</a>
+         </li> -->
+         <li class="py-2">
+            <a class="" type="button" data-bs-toggle="modal" data-bs-target="#change-password-modal">Change Password</a>
+         </li>
+         <li class="py-2">
+            <a class="" href="?logout=1" style="color: red !important;">Logout</a>
+         </li>
+
+      </ul>
+   </div>
+</div>
