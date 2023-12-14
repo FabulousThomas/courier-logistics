bbb1fd6b69bc404b6bf0f904ae4fd5791da3e0f8
Mba Thomas <angelfab631@gmail.com>
2023-12-14 22:37:53 +0300

    Update navbar.php

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,10 +1,10 @@
  <!-- NAV START -->
- <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-0 py-3 fixed-top shadow black-bg">
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-0 py-3 fixed-top shadow-sm black-bg">
    <div class="container text-dark">
      <a href="./index.php">
-       <img src="./assets/img/logo/logo.png" width="100px" alt="LOGO">
+       <img src="./assets/img/logo/logo.png" width="100px" height="40px" alt="LOGO">
      </a>
-     <button class="navbar-toggler d-lg-none d-inline theme-color" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+     <button class="navbar-toggler d-lg-none d-inline theme-color shadow-none border-0" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
        <span class="navbar-toggler-icon theme-color"></span>
      </button>
      <div class="collapse navbar-collapse" id="collapsibleNavId">
@@ -26,6 +26,10 @@
 
          <li class="nav-item">
            <a class="nav-link" href="./track-package.php">Track</a>
+         </li>
+
+         <li class="nav-item">
+           <small class="text-white shadow-none" id="google_translate_element"></small>
          </li>
 
        </ul>
