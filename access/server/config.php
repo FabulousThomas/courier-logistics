f99c3e2b1981cd51020d955ea969113da096e9fe
Mba Thomas <angelfab631@gmail.com>
2023-12-14 22:36:46 +0300

    Update config.php

--- access/server/config.php
+++ access/server/config.php
@@ -10,9 +10,12 @@ define('APPROOT', dirname(dirname(dirname(__FILE__))));
 // App root
 define('ADMINROOT', dirname(dirname(__FILE__)));
 // Url Root
-define('URLROOT', 'http://localhost/maxcourier');
+define('URLROOT', 'http://localhost/courier-logistics');
 // Site Name
-define('SITENAME', 'MaxExpress Services');
+define('SITENAME', 'JETKLIN LOGISTICS');
+define('EMAIL', 'jetklin-logistics@mail.com');
+define('WEBSITE', 'jetklinlogistics.com');
+define('PHONE', '+15645246902');
 // Site Logo
 define('SITELOGO', '<h5 class="text-uppercase mb-0" style="color: white;">-FreeTown&acd;</h5>
 <h5 class="text-uppercase mb-0 text-white"> &ThinSpace; &ThinSpace; &ThinSpace; Hotel&Suit-</h5>');
